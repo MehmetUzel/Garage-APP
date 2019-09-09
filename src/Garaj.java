560f968535f2c713914be83efd175d9efd9f9f6e
Mehmet Uzel <98mehmetuzel@gmail.com>
2019-09-09 12:09:52 +0300

    Add files via upload

---
+++ src/Garaj.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garaj {
+
+    private List<Arac> garaj = new ArrayList<>(); /* Garajdaki araçlarımızı burada tutuyoruz.*/
+
+    private int garajBoyutu;
+
+    public Garaj(int garajBoyutu, List<Arac> garaj){
+        this.garajBoyutu = garajBoyutu;
+        this.garaj = garaj;
+    }
+
+    public  List<Arac> getGaraj() { return garaj; }
+
+    public void  setGaraj(List<Arac> newGaraj) {garaj = newGaraj;}
+
+    public int findAracByTip(int tip) {
+        for (Arac a : this.garaj){
+            if (a.getTip() == tip){
+               return this.garaj.indexOf(a);
+            }
+        }
+        return -1;
+    }
+
+    public int getGarajBoyutu() {return garajBoyutu;}
+
+    public void setGarajBoyutu(int newBoyut) {garajBoyutu = newBoyut;}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        int n; /*Kullanıcın belirleyeceği garaj boyutu*/
+      //  Arac[] garajimiz; /*Garajımızdaki araçları tutacağımız Arac arrayı*/
+
+        HashMap <Integer,String> aracIsim = new HashMap <Integer, String>() ; /*Aracların tiplerine göre isimleri*/
+        HashMap <Integer,Integer> aracHacim = new HashMap<Integer, Integer>(); /*Aracların tiplerine göre hacimleri*/
+
+        aracIsim.put(1,"Bisiklet");
+        aracIsim.put(2,"Motorsiklet");
+        aracIsim.put(3,"Araba");
+        aracIsim.put(4,"Kamyonet");
+        aracIsim.put(5,"Otobüs");
+        aracIsim.put(6,"Tır");
+
+        aracHacim.put(1,1);
+        aracHacim.put(2,1);
+        aracHacim.put(3,2);
+        aracHacim.put(4,3);
+        aracHacim.put(5,5);
+        aracHacim.put(6,8);
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("5 ile 50 arasında bir garaj boyutu giriniz : ");
+
+        n = sc.nextInt();
+
+        List <Arac> garajimiz = new ArrayList<>();
+
+        Garaj yenigaraj = new Garaj(n,garajimiz); /*Belirlenen boyutta yeni bir garaj objesi oluşturuyoruz.*/
+
+        System.out.println("Yapmak istediğiniz işlemi giriniz (GIR,CIK,LIST)");
+
+        int loop = -1 ;
+
+        while (loop < 0){  /*Kullanıcan sürekli girdi almamızı sağlayacak döngü*/
+
+           Scanner ac = new Scanner(System.in);
+           Scanner dc = new Scanner(System.in);
+                if (ac.hasNext("GIR")) {
+
+                    int tip = dc.nextInt();
+                    if(yenigaraj.getGarajBoyutu() >= aracHacim.get(tip))
+                    {
+                        yenigaraj.getGaraj().add(new Arac(tip, aracIsim.get(tip), aracHacim.get(tip)));
+                        yenigaraj.setGarajBoyutu(yenigaraj.getGarajBoyutu() - aracHacim.get(tip));
+                        System.out.println("Yeni garaj hacmi " + yenigaraj.getGarajBoyutu());
+                    }
+                    else
+                        {
+                            System.out.println("Garaj dolu lütfen araç çıkartın ya da daha küçük boyutlu bir araç giriniz.");
+                    }
+                } else if (ac.hasNext("LIST")) {
+                    Iterator iterator = yenigaraj.getGaraj().iterator();
+                    while(iterator.hasNext()){
+                        System.out.println(iterator.next());
+                    }
+                } else if (ac.hasNext("CIK")) {
+                    if (n!=yenigaraj.getGarajBoyutu()) {
+                        int tip = dc.nextInt();
+                        int index = yenigaraj.findAracByTip(tip);
+                        yenigaraj.getGaraj().remove(index);
+                        yenigaraj.setGarajBoyutu(yenigaraj.getGarajBoyutu() + aracHacim.get(tip));
+                        System.out.println("Yeni garaj hacmi " + yenigaraj.getGarajBoyutu());
+                    }
+                    else{
+                        System.out.println("Garaj boş abi neyi çıkarmamı bekliyorsun.");
+                    }
+                }
+
+        }
+
+    }
+
+
+}
